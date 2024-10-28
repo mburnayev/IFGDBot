b2560dde3ab20fa32e5d9f559d79f2d5f4d48938
mburnayev <misha@burnayev.com>
2024-10-27 22:53:04 -0400

    Fixed bugs, bot works 100%, just need to add more sfx

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ This bot was created as a joke per a friend's request, and I thought it would be
 
 ## Launching the bot
 - Clone this repository, make sure you have Node.js installed
-- Install dependencies: `npm install discord.js dotenv @discordjs/voice ffmpeg ffmpeg-static libsodium-wrappers`
+- Install dependencies: `npm install discord.js dotenv @discordjs/voice ffmpeg ffmpeg-static libsodium-wrappers get-audio-duration`
 - Some of these dependencies might not be necessary, will need to figure out which ones later
 - Create an `.env` file, add `DISCORD_TOKEN=<your app token>`
 - Bring the bot online with `node index.js` or simply `node .`
@@ -18,9 +18,12 @@ This bot was created as a joke per a friend's request, and I thought it would be
   - [x] If active, wait for user(s) to connect to a vc
   - [x] When user(s) are connected, choose and play a cutaway gag at random
   - [x] After cutaway gag ends, wait some random amount of time, then choose and play another cutaway gag
-- [ ] Fix bugs
+- [x] Fix bugs
   
 ## Bugs:
-- The audio player(s) are not correctly deallocated when the bot disconnects from/switches VCs, probably due to await/asychronous delay properties
-  - When the bot has been disconnected from a VC, this will produce unlimited errors, which are caught and ignored, as a result of phantom audio players trying to play SFX
-  - When the bot has switched/reconnected to a VC, this will result in different cutaways cutting each other off
+- ~~The audio player(s) are not correctly deallocated when the bot disconnects from/switches VCs, probably due to await/asychronous delay properties~~
+  - ~~When the bot has been disconnected from a VC, this will produce unlimited errors, which are caught and ignored, as a result of phantom audio players trying to play SFX~~
+  - ~~When the bot has switched/reconnected to a VC, this will result in different cutaways cutting each other off~~
+
+## Bug Resolutions:
+- I'm not quite sure why I opted to use asynchronous waits instead of just audio player callbacks... anyways, I added a callback that checks if the audio player is idle
--- index.js
+++ index.js
@@ -12,7 +12,7 @@
 // Imports
 const fs = require("fs");
 const { Client, GatewayIntentBits, SlashCommandBuilder } = require("discord.js");
-const { joinVoiceChannel, createAudioPlayer, createAudioResource } = require("@discordjs/voice");
+const { joinVoiceChannel, createAudioPlayer, createAudioResource, AudioPlayerStatus } = require("@discordjs/voice");
 
 // Initialize environment variables and sets commands
 require("dotenv").config();
@@ -26,12 +26,11 @@ const client = new Client({
 });
 const filesPath = "./assets/sounds/";
 const filesList = fs.readdirSync(filesPath);
+
 var peterStatus = false;
 var vcConnection = null;
-var audioPlayer = null;
+const audioPlayer = createAudioPlayer();
 var audioResource = null;
-
-const delay = ms => new Promise(res => setTimeout(res, ms));
 
 function initConnection(channel) {
     vcConnection = joinVoiceChannel({
@@ -43,30 +42,30 @@ function initConnection(channel) {
 }
 
 function terminateConnection() {
+    audioPlayer.stop();
+    audioResource = null;
     vcConnection.disconnect();
     vcConnection = null;
-    audioPlayer = null;
 }
 
-function initPlayer() {
-    audioPlayer = createAudioPlayer();
-    vcConnection.subscribe(audioPlayer);
+function subscribePlayer() {
+    vcSubscription = vcConnection.subscribe(audioPlayer);
 }
 
 function playSFX() {
-    var selectResource = filesList[Math.floor(Math.random() * (filesList.length - 1 + 1)) + 1];
+    var selectResource = filesList[Math.floor(Math.random() * filesList.length)];
     audioResource = createAudioResource(filesPath + selectResource);
     try {
         audioPlayer.play(audioResource);
     } catch (error) {
-        console.log("Caught illegal attempt to play SFX after bot disconnected");
+        console.log("Failed to play SFX");
     }
 }
 
 client.on("ready", (message) => {
     const pOn = new SlashCommandBuilder().setName("peter_on").setDescription("Turns Intrusive Family Guy on");
     const pOff = new SlashCommandBuilder().setName("peter_off").setDescription("Turns Intrusive Family Guy off");
-    const pPing = new SlashCommandBuilder().setName("peter_ping").setDescription("Checks if Intrusive Family Guy is online");
+    const pPing = new SlashCommandBuilder().setName("peter_ping").setDescription("Pings Intrusive Family Guy");
     client.application.commands.create(pOn);
     client.application.commands.create(pOff);
     client.application.commands.create(pPing);
@@ -79,37 +78,35 @@ client.on("interactionCreate", async (interaction) => {
     if (interaction.commandName === "peter_on") {
         peterStatus = true;
         await interaction.reply("You've made a grave mistake...");
-    } 
+    }
     else if (interaction.commandName === "peter_off") {
         peterStatus = false;
         if (vcConnection !== null) {
             terminateConnection();
         }
         await interaction.reply("Retep will be back...");
-    } 
+    }
     else if (interaction.commandName === "peter_ping") {
         await interaction.reply("nyehehe!");
     }
 })
 
-client.on("voiceStateUpdate", async (oldState, newState) => {
+client.on("voiceStateUpdate", (oldState, newState) => {
     const USER_LEFT = !newState.channel;
     const USER_JOINED = !oldState.channel;
     const USER_MOVED = (newState.channel?.id !== oldState.channel?.id) && !USER_JOINED && !USER_LEFT;
 
-    if (USER_JOINED && (peterStatus == true) && (vcConnection === null)) {
+    // If user joined and IFGDB hasn't spoken yet or user moved and IFGDB already spoke and DC'd, connect and play SFX
+    if ((USER_JOINED && (peterStatus == true) && (vcConnection === null)) || (USER_MOVED && (peterStatus == true) && (vcConnection === null))) {
         var currChannel = newState.guild.channels.cache.get(newState.channelId);
         var members = currChannel.members;
         if (members.size > 0) {
             initConnection(currChannel);
-            initPlayer();
-            while (true) {
-                playSFX();
-                await delay(Math.floor(Math.random() * (60000 - 40000 + 1)) + 40000);
-            }
+            subscribePlayer();
+            playSFX();
         }
     }
-    // Just disconnect and connect stitched together
+    // User switching while SFX playing is just disconnect and connect stitched together
     else if (USER_MOVED && (peterStatus == true) && (vcConnection !== null)) {
         var channel = oldState.guild.channels.cache.get(oldState.channelId);
         var members = channel.members;
@@ -120,19 +117,24 @@ client.on("voiceStateUpdate", async (oldState, newState) => {
         members = channel.members;
         if (members.size > 0) {
             initConnection(channel);
-            initPlayer();
-            while (true) {
-                playSFX();
-                await delay(Math.floor(Math.random() * (60000 - 40000 + 1)) + 40000);
-            }
+            subscribePlayer();
+            playSFX();
         }
     }
+    // If user leaves, IFGDB leaves as well
     else if (USER_LEFT && (vcConnection !== null)) {
         var currChannel = oldState.guild.channels.cache.get(oldState.channelId);
         var members = currChannel.members;
         if (members.size == 1) {
             terminateConnection();
         }
+    }
+})
+
+audioPlayer.on("stateChange", (oldState, newState) => {
+    // If audio player finishes playing SFX, disconnect
+    if (newState.status === AudioPlayerStatus.Idle) {
+        terminateConnection();
     }
 })
 
